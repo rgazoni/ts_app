d6f9528f84689928edb03ba3b359a4a6020deadb
Ramon Gazoni <ramongazonil@gmail.com>
2022-11-29 18:31:42 -0300

    catching errors during development

--- index.ts
+++ index.ts
@@ -2,6 +2,30 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(Response => {
-    console.log(Response.data);
+
+    const todo = Response.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    logTodo(id, title, completed);
+
 });
+
+
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+        The todo with ID: ${id}
+        Has a title of: ${title}
+        Is it completed? ${completed}
+    `);
+}
