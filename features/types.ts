f3f5104aec7a73b05349605f996ae5031d3b24b1
Ramon Gazoni <ramongazonil@gmail.com>
2022-12-01 18:24:47 -0300

    Types

---
+++ features/types.ts
@@ -0,0 +1,19 @@
+//today points to a Date Obj
+const today = new Date();
+today. getMonth;
+
+const person = {
+    age: 20,
+};
+
+class Color {}
+const red = new Color();
+
+
+
+
+
+
+
+
+//Type Inference -> Relying on TS to make the Type annotations work for us
